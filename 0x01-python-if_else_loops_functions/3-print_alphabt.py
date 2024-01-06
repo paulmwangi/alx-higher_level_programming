eb56bf6b1796d9bd18508ee18dc02fe830970971
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2024-01-06 20:56:05 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i is not 101 and i is not 113:
-        print("{:c}".format(i), end="")
+for char in range(ord('a'), ord('z') + 1):
+    if chr(char) not in 'qe':
+        print("{}".format(chr(char)), end="")
