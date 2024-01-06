21b40134c1424f17735834d7173c67fc2025b04c
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2024-01-06 20:57:24 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# We use i for the first digit and j for the second
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        if i is not 8 or j is not 9:
-            print("{:d}{:d}".format(i, j), end=", ")
+
+for x in range(10):
+    for y in range(x + 1, 10):
+        if x < 8:
+            print("{:d}{:d}, ".format(x, y), end="")
         else:
-            print("{:d}{:d}".format(i, j))
+            print("{:d}{:d}".format(x, y))
