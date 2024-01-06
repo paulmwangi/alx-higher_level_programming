2e17b339849117e06c6a6e6e12e30fde4bb0a1b8
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2024-01-06 21:13:09 +0300

    Update lists.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -19,6 +19,7 @@ typedef struct listint_s
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *insert_node(listint_t **head, int number);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
