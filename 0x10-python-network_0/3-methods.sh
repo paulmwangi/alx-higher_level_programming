a066cb5548e50c9fc3ba0ea6101f7bde963eb330
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2024-04-30 19:07:42 +0300

    Add files via upload

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  a Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -si "$1" | awk -F ": " '/Allow/ {print $2}'
+# display all HTTP methods the server will accept using curl.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
